66aa53bde25a0d4f198e547cd47fc4da637f259f
kaustubh812 <watanekaustubh@gmail.com>
2023-10-10 21:01:18 +0530

    closebutton made fixed to absolute

---
+++ script.js
@@ -0,0 +1,11 @@
+// Made two variables for open button & close button
+let openRulesTab = document.querySelector('rules_button');
+let closeRulesTab = document.querySelector('close_button');
+
+openRulesTab.addEventListener("click",function(){
+    document.body.classList.add("rulesTab-active");
+})
+
+closeRulesTab.addEventListener("click",function(){
+    document.body.classList.remove("rulesTab-active");
+})
