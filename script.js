c50aedbef10913bf26a79a5bd1753eb745154d21
kaustubh812 <watanekaustubh@gmail.com>
2023-10-10 21:13:56 +0530

    rules section activated

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Made two variables for open button & close button
-let openRulesTab = document.querySelector('rules_button');
-let closeRulesTab = document.querySelector('close_button');
+let openRulesTab = document.querySelector('#rules_button');
+let closeRulesTab = document.querySelector('#close_button');
 
 openRulesTab.addEventListener("click",function(){
     document.body.classList.add("rulesTab-active");
